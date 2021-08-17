e3d50213c811b63f8aa09a1540e4be2f289a27a8
roeisavion <68278395+roeisavion@users.noreply.github.com>
2021-08-17 05:45:03 +0300

    clients connecting to server

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import {Game} from '../Game/Game'
+import {Login} from '../login/login'
 import { createDeck, shuffleDeck } from '../deck.jsx'
 
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 
-let shuffledDeck = shuffleDeck(createDeck());
+const client = new W3CWebSocket('ws://127.0.0.1:9090');
+//let shuffledDeck = shuffleDeck(createDeck());
 
 function App() {
   return (
     <div className="App">
-      <Game shuffledDeck={shuffledDeck}/>
+      {/* <Game shuffledDeck={shuffledDeck}/> */}
+      <Login client={client}/>
     </div>
   );
 }
